c43a0475189bc5fb2691d754bc95ecef7f9ae115
Phiravit Imsuchart <100277519+Phiravit@users.noreply.github.com>
2024-07-05 23:21:30 +0700

    Add effect
    add when u click button it will show red effect with opacity

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  //when click add connt by 1
+  const handleClick = () => {
+    setCount((count) => count + 1);
+    const button = document.getElementById('colorButton');
+    if (button) {
+      button.classList.add('effect');
+      setTimeout(() => {
+        button.classList.remove('effect');
+      }, 200);
+    }
+  };
 
   return (
     <>
@@ -18,7 +30,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button id="colorButton" onClick={handleClick}>
           count is {count}
         </button>
         <p>
@@ -29,7 +41,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
