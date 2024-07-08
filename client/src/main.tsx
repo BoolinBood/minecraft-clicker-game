a4788734ffc1ffde8294fed666f120ef1cabc1d7
Phiravit Imsuchart <100277519+Phiravit@users.noreply.github.com>
2024-07-08 12:10:40 +0700

    add

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,46 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import SignInPage from "./pages/sign-in-page/SignInPage.tsx";
+import ClickerPage from "./pages/clicker-page/ClickerPage.tsx";
+import LeaderboardPage from "./pages/leaderboard-page/LeaderboardPage.tsx";
+import StorePage from "./pages/store-page/StorePage.tsx";
+import ConfirmPage from "./pages/confirm-page/ConfirmPage.tsx";
+import Trade from "./pages/trade-page/Trade.tsx";
+import SignUpPage from "./pages/sign-up-page/SignUpPage.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <SignInPage />,
+  },
+  {
+    path: '/signup',
+    element: <SignUpPage />
+  },
+  {
+    path: '/clicker',
+    element: <ClickerPage />
+  },
+  {
+    path: "/leaderboard",
+    element: <LeaderboardPage />,
+  },
+  {
+    path: "store",
+    element: <StorePage />,
+  },
+  { path: "confirm",
+    element: <ConfirmPage /> 
+  },
+  { path: "trade",
+    element: <Trade />
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ client/src/types.tsx
@@ -0,0 +1,36 @@
+export type User = {
+  id: number,
+  inventoryId: number,
+  username: string,
+  stats_coins: number,
+  stats_luck: number,
+  stats_damage: number,
+  destroyed_block: number,
+}
+
+export type Block = {
+  id: number,
+  name: string,
+  health: number,
+  spawnChance: number,
+}
+
+export type Item = {
+  id: number,
+  name: string,
+  tradeValue: number,
+  rarity_name: string,
+  rarity_chance: number,
+}
+
+
+export type Inventory = {
+  ownedBy: number,
+  itemId: number,
+  itemName: string,
+  amount: number,
+  marketInfo_onMarket: boolean,
+  marketInfo_price: number,
+}
+
+export type TradeReq = {}
